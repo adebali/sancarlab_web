b75597a3d83d4283637f60af77b6a0634d81ec29
Adebali <adebali@users.noreply.github.com>
2017-09-22 14:57:46 -0400

    no open pos

--- pages/index.vue
+++ pages/index.vue
@@ -10,12 +10,12 @@
 
     <welcome class="welcome">
     </welcome>
-    <div class="wrap text-center">
+    <!-- <div class="wrap text-center">
    <h2>Join Our Team! </h2>
     <p>
       We are hiring! View open positions <a href="/openpositions">here</a>.
     </p>
-    </div>
+    </div> -->
 
   </div>
 </template>
