03a662bbcad909765bec93d94d79242545a54f89
Adebali <adebali@users.noreply.github.com>
2017-11-01 23:01:39 -0400

    no more open position

--- components/welcome.vue
+++ components/welcome.vue
@@ -3,6 +3,7 @@
   <!-- <h1>Welcome!</h1> -->
   <p>
     Welcome to the Sancar laboratory website! We are a biochemistry lab at the University of North Carolina. Our lab studies DNA damage repair, cell cycle checkpoints, circadian clock, and the coupling of these processes in organisms ranging from E. coli to mice to human cells. DNA is damaged every day by numerous mechanisms, such as UV light. Successful repair of this damage is essential for healthy life. Our lab focusses specifically on nucleotide excision repair and its regulation by cell cycle checkpoints and the circadian clock. Understanding these processes and their interactions can help us better understand human disease and treatment response.
+    <a href="/research">Learn more </a>
   </p>
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -8,14 +8,16 @@
       </div>
     </div>
 
+    <hr>
     <welcome class="welcome">
     </welcome>
-    <div class="wrap text-center">
-   <h2>Join Our Team! </h2>
-    <p>
-      We are hiring! View open positions <nuxt-link to="/openpositions">here</nuxt-link>.
-    </p>
-    </div>
+    <hr>
+    <!-- <div class="wrap text-center"> -->
+   <!-- <h2>Join Our Team! </h2> -->
+    <!-- <p> -->
+      <!-- We are hiring! View open positions <nuxt-link to="/openpositions">here</nuxt-link>. -->
+    <!-- </p> -->
+    <!-- </div> -->
 
   </div>
 </template>
