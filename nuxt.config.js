b131f5bcfe975acba2371ac3fd8f353197a277cc
Adebali <adebali@users.noreply.github.com>
2018-02-01 22:13:51 -0500

    delete nuxt

--- nuxt.config.js
+++ nuxt.config.js
@@ -39,7 +39,7 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      { hid: 'description', name: 'description', content: 'Sancar Lab Offical Website' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/img/ramsey.png' },
